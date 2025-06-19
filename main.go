525bd54f818beb5a4f27d65b74081e86aa87a335
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2025-06-18 20:07:02 -0600

    Adjust Mandelbrot vertical range for 4:3 aspect

--- main.go
+++ main.go
@@ -90,8 +90,8 @@ func main() {
 	maxIter := uint32(100)
 	xmin := float32(-2.0)
 	xmax := float32(1.0)
-	ymin := float32(-1.2)
-	ymax := float32(1.2)
+	ymin := float32(-1.125)
+	ymax := float32(1.125)
 
 	bufSize := int(width * height * 8)
 	imgBuf, err := runner.CreateEmptyBuffer(cl.WRITE_ONLY, bufSize)
